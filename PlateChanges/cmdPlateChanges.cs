63b12a284afd785413228ab259297a148c9be24a
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2025-08-02 18:52:40 -0500

    Add project files.

---
+++ PlateChanges/cmdPlateChanges.cs
@@ -0,0 +1,41 @@
+﻿namespace PlateChanges
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdPlateChanges : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document curDoc = uidoc.Document;
+
+            // get data for the form
+
+            // launch the form
+
+            // get data from the form
+
+            // process the data from the form
+
+            return Result.Succeeded;
+        }
+
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            Common.ButtonDataClass myButtonData = new Common.ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData.Data;
+        }
+    }
+}
